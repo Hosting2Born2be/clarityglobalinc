1f75c005e5bd92b34cab0d060eff3399fa3bcdf9
daniil-b2be <dan.developer@born2be.media>
2025-02-22 11:04:26 +0200

    added scss

--- src/widgets/privacy-policy/components/privacy-content/privacy-content.tsx
+++ src/widgets/privacy-policy/components/privacy-content/privacy-content.tsx
@@ -2,7 +2,7 @@
 
 import { ReactNode } from 'react';
 
-import st from './privacy-content.module.css';
+import st from './privacy-content.module.scss';
 
 export function PrivacyContent({ children }: { children: ReactNode }) {
   return <section className={st.layout}>{children}</section>;
